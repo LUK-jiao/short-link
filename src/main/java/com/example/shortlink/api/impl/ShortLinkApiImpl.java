fa3c5709ee0642a67161faee7a1cfa0e6423b581
LUK-jiao <jiao_zeming@163.com>
2026-04-29 21:45:40 +0800

    feat: 添加 Spring @Service 注解到 ShortLinkApiImpl 类

--- src/main/java/com/example/shortlink/api/impl/ShortLinkApiImpl.java
+++ src/main/java/com/example/shortlink/api/impl/ShortLinkApiImpl.java
@@ -11,9 +11,11 @@ import com.linkflow.api.dto.shortlink.ShortLinkResultDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @DubboService
 @Slf4j
+@Service
 public class ShortLinkApiImpl implements ShortLinkApi {
 
     @Autowired
